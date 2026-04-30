70e86cfd65a2b97cf1b4fa1d4f83f70cd488d0e6
Marcin Syngajewski <archetelecynacja@gmail.com>
2026-04-30 22:37:57 +0300

    init

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,9 @@
 import { defineConfig } from "astro/config";
 
 export default defineConfig({
-  site: "https://archetylator.github.io"
+  site: "https://archetylator.github.io",
+  server: {
+    host: "0.0.0.0",
+    port: 4321
+  }
 });
